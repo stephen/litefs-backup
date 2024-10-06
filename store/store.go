808ca5b3a24f6f8e3f35d2ccf5429c2b4a7d3d63
Stephen Wan <stephen@stephenwan.com>
2024-10-06 16:59:06 +0900

    store: increase default level 2 retention to 30 days
    Otherwise, we only get 1-hour granularity for the past month.

--- store/store.go
+++ store/store.go
@@ -58,7 +58,7 @@ func NewStore(config *lfsb.Config) *Store {
 		Levels: []*CompactionLevel{
 			{Level: 0},
 			{Level: 1, Interval: 10 * time.Second, Retention: 1 * time.Hour},
-			{Level: 2, Interval: 5 * time.Minute, Retention: 3 * 24 * time.Hour},
+			{Level: 2, Interval: 5 * time.Minute, Retention: 30 * 24 * time.Hour},
 			{Level: 3, Interval: 1 * time.Hour, Retention: 30 * 24 * time.Hour},
 		},
 		Now: func() time.Time {
